da89205811eefd722cddd2db8e896bfe73f97b8e
andi-abdullah-pangeran <andipangerang1989@gmail.com>
2024-02-16 16:58:40 +0700

    skip luar negri, and fix crawler path

--- analytics/schema/V3__create_view_clean_suara_pemilu_2024.sql
+++ analytics/schema/V3__create_view_clean_suara_pemilu_2024.sql
@@ -4,6 +4,6 @@ SELECT
 FROM
     suara_pemilu_2024
 WHERE
-    (suara_sah = 0 AND (total_suara_capres_01 + total_suara_capres_02 + total_suara_capres_03) <= 300)
+    (kode_prov != '99' AND suara_sah = 0 AND (total_suara_capres_01 + total_suara_capres_02 + total_suara_capres_03) <= 300)
    OR
-    (suara_sah > 0 AND suara_sah >= (total_suara_capres_01 + total_suara_capres_02 + total_suara_capres_03));
+    (kode_prov != '99' AND  suara_sah > 0 AND suara_sah >= (total_suara_capres_01 + total_suara_capres_02 + total_suara_capres_03));
